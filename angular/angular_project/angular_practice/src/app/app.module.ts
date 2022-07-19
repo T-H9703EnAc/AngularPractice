016c0cf5c3ece46c65d769a0d30a5c6f7d8726e9
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2022-07-20 06:22:08 +0900

    修正

--- angular/angular_project/angular_practice/src/app/app.module.ts
+++ angular/angular_project/angular_practice/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
