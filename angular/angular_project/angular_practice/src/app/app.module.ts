79c8a49fdf3875e7d1277e474b3d720facc76bf5
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2022-07-24 02:26:35 +0900

    修正

--- angular/angular_project/angular_practice/src/app/app.module.ts
+++ angular/angular_project/angular_practice/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MembersComponent
+    MembersComponent,
+    MemberDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule // ngModelの使うのに必要
+    FormsModule,// ngModelの使うのに必要
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/angular_project/angular_practice/src/app/members/members.component.ts
+++ angular/angular_project/angular_practice/src/app/members/members.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member'
-import { MEMBERS } from "../mock.members"
+import { MemberService } from "../member.service"
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-members',
@@ -8,19 +9,26 @@ import { MEMBERS } from "../mock.members"
   styleUrls: ['./members.component.css']
 })
 export class MembersComponent implements OnInit {
-  members = MEMBERS;
-  member: Member =  {
-    id:1,
-    name: '田中太郎'
-  };
+  members!:Member[];
   // selectedMemberだとNullを許容しない selectedMember!だとNullを許容する
   selectedMember!: Member;
-  constructor() {}
+  
+  constructor(
+    private memberService: MemberService,
+    private messageService: MessageService,   
+    ) {}
+  
+  ngOnInit(): void {
+    this.getMember();
+  }
   onSelect(member: Member):void{
     this.selectedMember = member;
+    this.messageService.add(`MemberComponent: 社員データ(id=${member.id}が選択されました。)`)
   }
-
-  ngOnInit(): void {
+  getMember():void{
+    this.memberService.getMembers()
+    // of(Members)からMembersが送られてくる(非同期処理)
+    .subscribe(members =>this.members = members);
   }
 
 }
